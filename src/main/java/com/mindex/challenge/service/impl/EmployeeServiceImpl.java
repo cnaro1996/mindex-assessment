9e3059adaf32a4a4a8ce28054f3e346727aa3a41
cnaro1996 <cnaro1996@gmail.com>
2024-07-22 16:11:17 -0400

    Refactor naive solution into optimized solution; Finish Task 1

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,14 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -45,5 +48,40 @@ public class EmployeeServiceImpl implements EmployeeService {
         LOG.debug("Updating employee [{}]", employee);
 
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public ReportingStructure getReportingStructure(String employeeId) {
+        LOG.debug("Getting reporting structure for employee with id [{}]", employeeId);
+
+        Employee employee = employeeRepository.findByEmployeeId(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("No employee found with employeeId: " + employeeId);
+        }
+
+        ReportingStructure reportingStructure = new ReportingStructure();
+        reportingStructure.setEmployee(employee);
+        reportingStructure.setNumberOfReports(calculateNumberOfReports(employee));
+
+        return reportingStructure;
+    }
+
+    /**
+     * Recursively calculates the total number of reports under a given employee. The total number of reports is
+     * determined by aggregating the number of directReports for an employee and all of their distinct reports.
+     */
+    private int calculateNumberOfReports(Employee employee) {
+        if(employee.getDirectReports() == null || employee.getDirectReports().isEmpty()) {
+            return 0;
+        }
+
+        int reports = employee.getDirectReports().size();
+        for(Employee directReport : employee.getDirectReports()) {
+            // The directReports entries are Employee objects with unpopulated directReports fields.
+            Employee populatedDirectReport = employeeRepository.findByEmployeeId(directReport.getEmployeeId());
+            reports += calculateNumberOfReports(populatedDirectReport);
+        }
+
+        return reports;
     }
 }
